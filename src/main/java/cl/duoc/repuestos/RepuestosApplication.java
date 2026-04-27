e52c09b7693be305a62c6c38a111c23143b6181c
unknown <Cetecom@SJAPCA03LC0702.duoc.acad>
2026-04-27 11:23:04 -0400

    commit inicial

---
+++ src/main/java/cl/duoc/repuestos/RepuestosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/repuestos/RepuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.repuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
