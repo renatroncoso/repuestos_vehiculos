d75d855828f56576642a3e985d9ba6ae1ca9cee3
unknown <Cetecom@SJAPCA03LC0726.duoc.acad>
2026-04-28 11:19:39 -0400

    se agregaron los script SQL

---
+++ src/main/resources/db/migration/V2__insert_users.sql
@@ -0,0 +1,18 @@
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'consumible' , 'libricante', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'consumible' , 'frenos', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'mecanicos' , 'trasmicion', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'mecanico' , 'suspencion', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL ,2,
+'electrico' , 'arranque', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'electrico' , 'focos', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL ,2,
+'carroceria' , 'exterior', NULL ,2);
+INSERT INTO REPUESTOS VALUES (NULL , 2,
+'carroceria' , 'interior', NULL ,2);
+
+COMMIT;
